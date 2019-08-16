1ab070933b2a99c3777c9c2c464aff544194e043
Evan Krone <evankrone1@gmail.com>
2019-08-16 17:36:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,8 +42,8 @@ def using_flatten(instruments)
   flat_array = instruments.flatten
 end
   
-def using_delete (instructors, steven)
-  no_offense_steven = insructors.delete(5)
+def using_delete (instructors, no_offense_steven)
+  no_offense_steven = insructors.delete("Steven")
 end
 
 
