ed620ec8a0fe2490c413ffe42a027f4cb808e203
Evan Krone <evankrone1@gmail.com>
2019-08-16 15:58:46 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,11 @@
+def using_push (array, string)
+  array = ["dogs", "cats", "birds"] 
+updated_array = array.push ("violet")
+p updated_array
+end
+
+def using_unshift (bouroughs_in_nyc, new_neighborhood)
+   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   new_neighborhood = "Staten Island"
+   updated_array = bouroughs_in_nyc.unshift (new_neighborhood)
+end
